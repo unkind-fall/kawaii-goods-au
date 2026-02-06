602cc2a39db6ac4d8941376ca7a083e281c38cb8
Sean <sean@MM.local>
2026-02-06 18:18:49 +0800

    fix: enable SVG images for placehold.co

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,8 @@ initOpenNextCloudflareForDev();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "picsum.photos" },
